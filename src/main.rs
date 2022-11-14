1435e9ad67f8b481d13ce8137844913eb75a7ab8
V13Axel <alex@stechstudio.com>
2022-11-14 15:44:27 -0500

    Looks like you can call drop() manually to avoid borrow issues

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::cell::RefCell;
+
 fn main() {
     println!("Hello, world!");
+}
+
+thread_local! {
+    static SOME_REF_CELL: RefCell<Vec<String>> = RefCell::new(vec![]);
+}
+
+#[test]
+fn test_refcell_threadlocal_replacement_thoughts() {
+    SOME_REF_CELL.with(|some_ref_cell| {
+        let mut borrowed_cell = some_ref_cell.borrow_mut();
+
+        borrowed_cell.push("Testing".into());
+
+        let mut testing_similar: Vec<String> = Vec::new();
+        testing_similar.push(String::from("Testing"));
+
+        assert_eq!(borrowed_cell.len(), testing_similar.len());
+
+        borrowed_cell.iter().enumerate().for_each(|(i, item)| {
+            assert_eq!(item.to_owned(), testing_similar[i]);
+        });
+
+        drop(borrowed_cell);
+
+        some_ref_cell.replace(testing_similar);
+    });
 }
